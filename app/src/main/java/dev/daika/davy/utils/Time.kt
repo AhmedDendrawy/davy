b77dfd25eaaca46a36aeb7f116f680620666fc9f
Ahmed Dendrawy <ahmeddendriwy11@gmail.com>
2026-09-20 22:42:47 +0300

    fix(player): use Duration to fix missing hours in time indicator

--- app/src/main/java/dev/daika/davy/utils/Time.kt
+++ app/src/main/java/dev/daika/davy/utils/Time.kt
@@ -1,14 +1,15 @@
 package dev.daika.davy.utils
 
-fun formatTime(ms: Long, needHours: Boolean = false): String {
+import kotlin.time.Duration.Companion.milliseconds
+
+fun formatTime(ms: Long): String {
     if (ms < 0) return "00:00"
-    val totalSeconds = ms / 1000
-    val totalMinutes = totalSeconds / 60
-    val hours = totalMinutes / 60
-    val minutes = totalMinutes % 60
-    val seconds = totalSeconds % 60
-    if (needHours) {
-        return String.format("%02d:%02d:%02d", hours, minutes, seconds)
+
+    return ms.milliseconds.toComponents { hours, minutes, seconds, _ ->
+        if (hours > 0) {
+            String.format("%02d:%02d:%02d", hours, minutes, seconds)
+        } else {
+            String.format("%02d:%02d", minutes, seconds)
+        }
     }
-    return String.format("%02d:%02d", minutes, seconds)
 }
